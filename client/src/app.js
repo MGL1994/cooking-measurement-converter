a144f9bf2663aaa2c648deb481ac7efc26875757
MGL1994 <michaelgl1994@gmail.com>
2020-04-12 17:38:32 +0100

    Added flour and sugar routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,25 @@
-var express = require('express');
-var router = express.Router()
+const express = require('express')
+
+const router = express.Router()
+
+function flourConversion(cups) {
+    return (cups * 125) + ' grams'
+}
+
+function sugarConversion(cups) {
+    return (cups * 200) + ' grams'
+}
 
 router.get("/", async (req, res) => res.send("Convert this!"))
+
+router.post("/flour", (req, res, next) => {
+    let amount = req.body.Cups
+    res.send(flourConversion(amount))
+})
+
+router.post("/sugar", (req, res) => {
+    let amount = req.body.Cups
+    res.send(sugarConversion(amount))
+})
 
 module.exports = router
