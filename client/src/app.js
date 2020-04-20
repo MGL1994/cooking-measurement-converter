b63d530576207a95c66233c0b98082ad1908f54f
MGL1994 <michaelgl1994@gmail.com>
2020-04-20 15:48:25 +0100

    Refactored routing

--- client/src/app.js
+++ client/src/app.js
@@ -3,38 +3,43 @@ const createError = require('http-errors')
 
 const router = express.Router()
 
+function conversionSelector(ingredient, amount) {
+
+    switch(ingredient) {
+        case 'flour':
+            return flourConversion(amount)
+        case 'sugar':
+            return sugarConversion(amount)
+    }
+}
+
 function flourConversion(cups) {
+
     return (cups * 125) + ' grams'
 }
 
 function sugarConversion(cups) {
+
     return (cups * 200) + ' grams'
 }
 
 router.get("/", (req, res) => res.send("Convert this!"))
 
-router.post("/flour", (req, res) => {
-    let amount = req.body.Cups
+router.post("/:id", (req, res) => {
+    
+    let ingredientReq = req.path.replace(/\//g, '')
+    let amountReq = req.body.Cups
+    let conversion = conversionSelector(ingredientReq, amountReq)
 
-    if(!amount || amount <= 0) {
+    if(!amountReq || amountReq <= 0) {
         throw createError(400, 'No amount entered!')
-    } else if(isNaN(amount)) {
-        throw createError(400, `'${amount}' is not a number!`)
+    } else if(isNaN(amountReq)) {
+        throw createError(400, `'${amountReq}' is not a number!`)
     } else {
-        res.send(flourConversion(amount))
+        conversion 
     }
-})
 
-router.post("/sugar", (req, res) => {
-    let amount = req.body.Cups
-
-    if(!amount || amount <= 0) {
-        throw createError(400, 'No amount entered!')
-    } else if(isNaN(amount)) {
-        throw createError(400, `'${amount}' is not a number!`)
-    } else {
-        res.send(sugarConversion(amount))
-    }
+    res.send(conversion)
 })
 
 router.use((err, req, res, next) => {
